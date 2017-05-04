cc5c5c042307235009ab518fdaf2dfc9fb13fba8
printfoo <shan.sam.jiang@gmail.com>
2017-05-03 20:32:21 -0400

    update

---
+++ 0344/reverse_string_stupid.py
@@ -0,0 +1,24 @@
+"""
+Stupid solution for Reverse String, O(n).
+
+Idea:
+Diectly reverse.
+"""
+
+# Solution.
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        out_s = []
+        len_s = len(s)
+        for i in range(len_s):
+            out_s.append(s[len_s - 1 - i])
+        return "".join(out_s)
+
+# Main.
+if __name__ == "__main__":
+    s = "hello"
+    print Solution().reverseString(s)
